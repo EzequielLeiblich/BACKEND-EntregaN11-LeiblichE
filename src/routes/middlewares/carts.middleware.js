3b11128589c99fabe15aada1ff2421afa566c01b
Ezequiel Leiblich <113488651+EzequielLeiblich@users.noreply.github.com>
2023-08-30 23:12:05 -0300

    Delete src directory

--- src/routes/middlewares/carts.middleware.js
+++
@@ -1,7 +0,0 @@
-export const verificarPertenenciaCarrito = (req,res,next) => {
-    if(req.user.cart === req.params.cid) {
-        next()
-    } else {
-        res.send("Solo puede agregar productos a su carrito")
-    }
-}
